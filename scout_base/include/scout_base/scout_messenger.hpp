ae0c577ccd9a6163c7937f4e17f6e1822317e2de
hjinnkim <khj12203@gmail.com>
2022-06-15 00:12:19 +0900

    Edit only for publishing Odometry

--- scout_base/include/scout_base/scout_messenger.hpp
+++ scout_base/include/scout_base/scout_messenger.hpp
@@ -17,7 +17,7 @@
 // #include <tf/transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 
-#include "scout_msgs/ScoutLightCmd.h"
+// #include "scout_msgs/ScoutLightCmd.h"
 #include "ugv_sdk/mobile_robot/scout_robot.hpp"
 #include <mutex>
 
@@ -35,11 +35,11 @@ public:
     bool pub_tf;
 
     bool simulated_robot_ = false;
-    int sim_control_rate_ = 50;
+    // int sim_control_rate_ = 50;
 
     void SetupSubscription();
 
-    void PublishStateToROS();
+    // void PublishStateToROS();
     void PublishSimStateToROS(double linear, double angular);
 
     void GetCurrentMotionCmdForSim(double &linear, double &angular);
@@ -52,10 +52,10 @@ private:
     geometry_msgs::Twist current_twist_;
 
     ros::Publisher odom_publisher_;
-    ros::Publisher status_publisher_;
-    ros::Publisher BMS_status_publisher_;
+    // ros::Publisher status_publisher_;
+    // ros::Publisher BMS_status_publisher_;
     ros::Subscriber motion_cmd_subscriber_;
-    ros::Subscriber light_cmd_subscriber_;
+    // ros::Subscriber light_cmd_subscriber_;
     tf2_ros::TransformBroadcaster tf_broadcaster_;
 
     // speed variables
@@ -69,7 +69,7 @@ private:
     ros::Time current_time_;
 
     void TwistCmdCallback(const geometry_msgs::Twist::ConstPtr &msg);
-    void LightCmdCallback(const scout_msgs::ScoutLightCmd::ConstPtr &msg);
+    // void LightCmdCallback(const scout_msgs::ScoutLightCmd::ConstPtr &msg);
     void PublishOdometryToROS(double linear, double angular, double dt);
 };
 } // namespace westonrobot
--- scout_base/src/scout_messenger.cpp
+++ scout_base/src/scout_messenger.cpp
@@ -27,14 +27,14 @@ namespace westonrobot
   {
     // odometry publisher
     odom_publisher_ = nh_->advertise<nav_msgs::Odometry>(odom_topic_name_, 50);
-    status_publisher_ = nh_->advertise<scout_msgs::ScoutStatus>("/scout_status", 10);
-    BMS_status_publisher_ = nh_->advertise<scout_msgs::ScoutBmsStatus>("/BMS_status", 10);
+    // status_publisher_ = nh_->advertise<scout_msgs::ScoutStatus>("/scout_status", 10);
+    // BMS_status_publisher_ = nh_->advertise<scout_msgs::ScoutBmsStatus>("/BMS_status", 10);
 
     // cmd subscriber
     motion_cmd_subscriber_ = nh_->subscribe<geometry_msgs::Twist>(
         "/cmd_vel", 5, &ScoutROSMessenger::TwistCmdCallback, this);
-    light_cmd_subscriber_ = nh_->subscribe<scout_msgs::ScoutLightCmd>(
-        "/scout_light_control", 5, &ScoutROSMessenger::LightCmdCallback, this);
+    // light_cmd_subscriber_ = nh_->subscribe<scout_msgs::ScoutLightCmd>(
+    //     "/scout_light_control", 5, &ScoutROSMessenger::LightCmdCallback, this);
   }
 
   void ScoutROSMessenger::TwistCmdCallback(
@@ -42,7 +42,7 @@ namespace westonrobot
   {
     if (!simulated_robot_)
     {
-      scout_->SetMotionCommand(msg->linear.x, msg->angular.z);
+      // scout_->SetMotionCommand(msg->linear.x, msg->angular.z);
     }
     else
     {
@@ -60,157 +60,155 @@ namespace westonrobot
     angular = current_twist_.angular.z;
   }
 
-  void ScoutROSMessenger::LightCmdCallback(
-      const scout_msgs::ScoutLightCmd::ConstPtr &msg)
-  {
-    if (!simulated_robot_)
-    {
-      if (msg->enable_cmd_light_control)
-      {
-        LightCommandMessage cmd;
+  // void ScoutROSMessenger::LightCmdCallback(
+  //     const scout_msgs::ScoutLightCmd::ConstPtr &msg)
+  // {
+  //   if (!simulated_robot_)
+  //   {
+  //     if (msg->enable_cmd_light_control)
+  //     {
+  //       LightCommandMessage cmd;
 
-        switch (msg->front_mode)
-        {
-          case scout_msgs::ScoutLightCmd::LIGHT_CONST_OFF:
-          {
-            cmd.front_light.mode = CONST_OFF;
-            break;
-          }
-          case scout_msgs::ScoutLightCmd::LIGHT_CONST_ON:
-          {
-            cmd.front_light.mode = CONST_ON;
-            break;
-          }
-          case scout_msgs::ScoutLightCmd::LIGHT_BREATH:
-          {
-            cmd.front_light.mode = BREATH;
-            break;
-          }
-          case scout_msgs::ScoutLightCmd::LIGHT_CUSTOM:
-          {
-            cmd.front_light.mode = LightMode::CUSTOM;
-            cmd.front_light.custom_value = msg->front_custom_value;
-            break;
-          }
-        }
+  //       switch (msg->front_mode)
+  //       {
+  //         case scout_msgs::ScoutLightCmd::LIGHT_CONST_OFF:
+  //         {
+  //           cmd.front_light.mode = CONST_OFF;
+  //           break;
+  //         }
+  //         case scout_msgs::ScoutLightCmd::LIGHT_CONST_ON:
+  //         {
+  //           cmd.front_light.mode = CONST_ON;
+  //           break;
+  //         }
+  //         case scout_msgs::ScoutLightCmd::LIGHT_BREATH:
+  //         {
+  //           cmd.front_light.mode = BREATH;
+  //           break;
+  //         }
+  //         case scout_msgs::ScoutLightCmd::LIGHT_CUSTOM:
+  //         {
+  //           cmd.front_light.mode = LightMode::CUSTOM;
+  //           cmd.front_light.custom_value = msg->front_custom_value;
+  //           break;
+  //         }
+  //       }
 
-        switch (msg->rear_mode)
-        {
-          case scout_msgs::ScoutLightCmd::LIGHT_CONST_OFF:
-          {
-            cmd.rear_light.mode = CONST_OFF;
-            break;
-          }
-          case scout_msgs::ScoutLightCmd::LIGHT_CONST_ON:
-          {
-            cmd.rear_light.mode = CONST_ON;
-            break;
-          }
-          case scout_msgs::ScoutLightCmd::LIGHT_BREATH:
-          {
-            cmd.rear_light.mode = BREATH;
-            break;
-          }
-          case scout_msgs::ScoutLightCmd::LIGHT_CUSTOM:
-          {
-            cmd.rear_light.mode = CUSTOM;
-            cmd.rear_light.custom_value = msg->rear_custom_value;
-            break;
-          }
-        }
+  //       switch (msg->rear_mode)
+  //       {
+  //         case scout_msgs::ScoutLightCmd::LIGHT_CONST_OFF:
+  //         {
+  //           cmd.rear_light.mode = CONST_OFF;
+  //           break;
+  //         }
+  //         case scout_msgs::ScoutLightCmd::LIGHT_CONST_ON:
+  //         {
+  //           cmd.rear_light.mode = CONST_ON;
+  //           break;
+  //         }
+  //         case scout_msgs::ScoutLightCmd::LIGHT_BREATH:
+  //         {
+  //           cmd.rear_light.mode = BREATH;
+  //           break;
+  //         }
+  //         case scout_msgs::ScoutLightCmd::LIGHT_CUSTOM:
+  //         {
+  //           cmd.rear_light.mode = CUSTOM;
+  //           cmd.rear_light.custom_value = msg->rear_custom_value;
+  //           break;
+  //         }
+  //       }
 
-        scout_->SetLightCommand(cmd.front_light.mode,cmd.front_light.custom_value,cmd.rear_light.mode,cmd.rear_light.custom_value);
-      }
-      else
-      {
-        scout_->DisableLightControl();
-      }
-    }
-    else
-    {
-      std::cout << "simulated robot received light control cmd" << std::endl;
-    }
-  }
+  //       scout_->SetLightCommand(cmd.front_light.mode,cmd.front_light.custom_value,cmd.rear_light.mode,cmd.rear_light.custom_value);
+  //     }
+  //     else
+  //     {
+  //       scout_->DisableLightControl();
+  //     }
+  //   }
+  //   else
+  //   {
+  //     std::cout << "simulated robot received light control cmd" << std::endl;
+  //   }
+  // }
 
-  void ScoutROSMessenger::PublishStateToROS()
-  {
-    current_time_ = ros::Time::now();
-    double dt = (current_time_ - last_time_).toSec();
+  // void ScoutROSMessenger::PublishStateToROS()
+  // {
+  //   current_time_ = ros::Time::now();
+  //   double dt = (current_time_ - last_time_).toSec();
 
-    static bool init_run = true;
-    if (init_run)
-    {
-      last_time_ = current_time_;
-      init_run = false;
-      return;
-    }
+  //   static bool init_run = true;
+  //   if (init_run)
+  //   {
+  //     last_time_ = current_time_;
+  //     init_run = false;
+  //     return;
+  //   }
 
-    auto robot_state = scout_->GetRobotState();
-    auto actuator_state = scout_->GetActuatorState();
+  //   auto robot_state = scout_->GetRobotState();
+  //   auto actuator_state = scout_->GetActuatorState();
 
-    // publish scout state message
-    scout_msgs::ScoutStatus status_msg;
-    scout_msgs::ScoutBmsStatus bms_status;
+  //   // publish scout state message
+  //   scout_msgs::ScoutStatus status_msg;
+  //   scout_msgs::ScoutBmsStatus bms_status;
 
-    status_msg.header.stamp = current_time_;
-    status_msg.linear_velocity = robot_state.motion_state.linear_velocity;
-    status_msg.angular_velocity = robot_state.motion_state.angular_velocity;
-    status_msg.base_state = robot_state.system_state.vehicle_state;
-    status_msg.control_mode = robot_state.system_state.control_mode;
-    status_msg.fault_code = robot_state.system_state.error_code;
-    status_msg.battery_voltage = robot_state.system_state.battery_voltage;
-    status_msg.light_control_enabled = robot_state.light_state.enable_cmd_ctrl;
-    status_msg.front_light_state.mode = robot_state.light_state.front_light.mode;
-    status_msg.front_light_state.custom_value = robot_state.light_state.front_light.custom_value;
-    status_msg.rear_light_state.mode = robot_state.light_state.rear_light.mode;
-    status_msg.rear_light_state.custom_value = robot_state.light_state.rear_light.custom_value;
-    if(scout_->GetParserProtocolVersion() == ProtocolVersion::AGX_V1)
-    {
-        for (int i = 0; i < 4; ++i)
-        {
-            status_msg.motor_states[i].current = actuator_state.actuator_state[i].current;
-            status_msg.motor_states[i].rpm = actuator_state.actuator_state[i].rpm;
-            status_msg.motor_states[i].temperature = actuator_state.actuator_state[i].motor_temp;
-            status_msg.driver_states[i].driver_temperature = actuator_state.actuator_state[i].driver_temp;
-        }
-    }
-    else
-    {
-        for (int i = 0; i < 4; ++i)
-        {
-            status_msg.motor_states[i].current = actuator_state.actuator_hs_state[i].current;
-            status_msg.motor_states[i].rpm = actuator_state.actuator_hs_state[i].rpm;
-            status_msg.motor_states[i].temperature = actuator_state.actuator_ls_state[i].motor_temp;
-            status_msg.motor_states[i].motor_pose = actuator_state.actuator_hs_state[i].pulse_count;
-            status_msg.driver_states[i].driver_state = actuator_state.actuator_ls_state[i].driver_state;
-            status_msg.driver_states[i].driver_voltage = actuator_state.actuator_ls_state[i].driver_voltage;
-            status_msg.driver_states[i].driver_temperature = actuator_state.actuator_ls_state[i].driver_temp;
-        }
+  //   status_msg.header.stamp = current_time_;
+  //   status_msg.linear_velocity = robot_state.motion_state.linear_velocity;
+  //   status_msg.angular_velocity = robot_state.motion_state.angular_velocity;
+  //   status_msg.base_state = robot_state.system_state.vehicle_state;
+  //   status_msg.control_mode = robot_state.system_state.control_mode;
+  //   status_msg.fault_code = robot_state.system_state.error_code;
+  //   status_msg.battery_voltage = robot_state.system_state.battery_voltage;
+  //   status_msg.light_control_enabled = robot_state.light_state.enable_cmd_ctrl;
+  //   status_msg.front_light_state.mode = robot_state.light_state.front_light.mode;
+  //   status_msg.front_light_state.custom_value = robot_state.light_state.front_light.custom_value;
+  //   status_msg.rear_light_state.mode = robot_state.light_state.rear_light.mode;
+  //   status_msg.rear_light_state.custom_value = robot_state.light_state.rear_light.custom_value;
+  //   if(scout_->GetParserProtocolVersion() == ProtocolVersion::AGX_V1)
+  //   {
+  //       for (int i = 0; i < 4; ++i)
+  //       {
+  //           status_msg.motor_states[i].current = actuator_state.actuator_state[i].current;
+  //           status_msg.motor_states[i].rpm = actuator_state.actuator_state[i].rpm;
+  //           status_msg.motor_states[i].temperature = actuator_state.actuator_state[i].motor_temp;
+  //           status_msg.driver_states[i].driver_temperature = actuator_state.actuator_state[i].driver_temp;
+  //       }
+  //   }
+  //   else
+  //   {
+  //       for (int i = 0; i < 4; ++i)
+  //       {
+  //           status_msg.motor_states[i].current = actuator_state.actuator_hs_state[i].current;
+  //           status_msg.motor_states[i].rpm = actuator_state.actuator_hs_state[i].rpm;
+  //           status_msg.motor_states[i].temperature = actuator_state.actuator_ls_state[i].motor_temp;
+  //           status_msg.motor_states[i].motor_pose = actuator_state.actuator_hs_state[i].pulse_count;
+  //           status_msg.driver_states[i].driver_state = actuator_state.actuator_ls_state[i].driver_state;
+  //           status_msg.driver_states[i].driver_voltage = actuator_state.actuator_ls_state[i].driver_voltage;
+  //           status_msg.driver_states[i].driver_temperature = actuator_state.actuator_ls_state[i].driver_temp;
+  //       }
+  //   }
 
-
-    }
-
-//      bms_status.SOC = state.SOC;
-//      bms_status.SOH = state.SOH;
-//      bms_status.battery_voltage = state.bms_battery_voltage;
-//      bms_status.battery_current = state.battery_current;
-//      bms_status.battery_temperature = state.battery_temperature;
-//      bms_status.Alarm_Status_1 = state.Alarm_Status_1;
-//      bms_status.Alarm_Status_2 = state.Alarm_Status_2;
-//      bms_status.Warning_Status_1 = state.Warning_Status_1;
-//      bms_status.Warning_Status_2 = state.Warning_Status_2;
-      BMS_status_publisher_.publish(bms_status);
+  //     bms_status.SOC = state.SOC;
+  //     bms_status.SOH = state.SOH;
+  //     bms_status.battery_voltage = state.bms_battery_voltage;
+  //     bms_status.battery_current = state.battery_current;
+  //     bms_status.battery_temperature = state.battery_temperature;
+  //     bms_status.Alarm_Status_1 = state.Alarm_Status_1;
+  //     bms_status.Alarm_Status_2 = state.Alarm_Status_2;
+  //     bms_status.Warning_Status_1 = state.Warning_Status_1;
+  //     bms_status.Warning_Status_2 = state.Warning_Status_2;
+  //     BMS_status_publisher_.publish(bms_status);
 
 
-    status_publisher_.publish(status_msg);
+  //   status_publisher_.publish(status_msg);
 
 
-    // publish odometry and tf
-    PublishOdometryToROS(robot_state.motion_state.linear_velocity, robot_state.motion_state.angular_velocity, dt);
+  //   // publish odometry and tf
+  //   PublishOdometryToROS(robot_state.motion_state.linear_velocity, robot_state.motion_state.angular_velocity, dt);
 
-    // record time for next integration
-    last_time_ = current_time_;
-  }
+  //   // record time for next integration
+  //   last_time_ = current_time_;
+  // }
 
   void ScoutROSMessenger::PublishSimStateToROS(double linear, double angular)
   {
@@ -226,21 +224,21 @@ namespace westonrobot
       return;
     }
 
-    // publish scout state message
-    scout_msgs::ScoutStatus status_msg;
+    // // publish scout state message
+    // scout_msgs::ScoutStatus status_msg;
 
-    status_msg.header.stamp = current_time_;
+    // status_msg.header.stamp = current_time_;
 
-    status_msg.linear_velocity = linear;
-    status_msg.angular_velocity = angular;
+    // status_msg.linear_velocity = linear;
+    // status_msg.angular_velocity = angular;
 
-    status_msg.base_state = 0x00;
-    status_msg.control_mode = 0x01;
-    status_msg.fault_code = 0x00;
-    status_msg.battery_voltage = 29.5;
-    status_msg.light_control_enabled = false;
+    // status_msg.base_state = 0x00;
+    // status_msg.control_mode = 0x01;
+    // status_msg.fault_code = 0x00;
+    // status_msg.battery_voltage = 29.5;
+    // status_msg.light_control_enabled = false;
 
-    status_publisher_.publish(status_msg);
+    // status_publisher_.publish(status_msg);
 
     // publish odometry and tf
     PublishOdometryToROS(linear, angular, dt);
